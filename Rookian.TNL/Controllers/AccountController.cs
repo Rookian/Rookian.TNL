73d41481105cd8e8b03d25144c8d4e81867d7704
Rookian <Rookian@gmx.de>
2014-12-12 18:29:12 +0100

    account feature

---
+++ Rookian.TNL/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using Rookian.TNL.Features.Account;
+
+namespace Rookian.TNL.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult LoginForm()
+        {
+            return View(new LoginForm());
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginForm loginForm)
+        {
+            return new EmptyResult();
+        }
+    }
+}
